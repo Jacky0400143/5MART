eeb33a28727236088208fc5f665b432b1174e98c
Yuan <asdasd789tw@gmail.com>
2023-09-03 01:11:17 +0800

    Main-Project_20230903_First-commit

---
+++ SpringEEIT69test/src/main/java/com/ispan/eeit69/SpringEeit69testApplication.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEeit69testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEeit69testApplication.class, args);
+	}
+
+}
---
+++ SpringEEIT69test/src/test/java/com/ispan/eeit69/SpringEeit69testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEeit69testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
